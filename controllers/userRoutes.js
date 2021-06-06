c35c2f750f6af0186d6810978a545dadee1c4c1d
Ezequiel Herrera <thebearginger@gmail.com>
2021-06-06 14:29:57 -0600

    saved

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 const User = require ("../models/user");
 
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: {
+      exclude: ["password"],
+    },
+  })
+    .then((userData) => res.json(userData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/getBlogs")
+
 router.post("/", (req, res) => {
     User.create({
         username: req.body.username,
@@ -22,5 +37,16 @@ router.post("/", (req, res) => {
     res.status(500).json(err);
 });
 });
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 
 module.exports = router;
